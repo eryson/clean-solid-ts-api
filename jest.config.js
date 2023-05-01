1efaccc6fc3928374aeb38b49fb6bb6b79f13ee1
Eryson <eryson.ktm@gmail.com>
2023-05-01 14:36:54 -0300

    test: ensure SignUpController return an error if no name is provided

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  roots: ["<rootDir>/__tests__"],
-  // collectCoverage: false,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  // coverageProvider: "v8",
-  testEnvironment: "node",
+  roots: ['<rootDir>/__tests__'],
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest",
+    '.+\\.ts$': 'ts-jest'
   },
-  // moduleNameMapper: {
-  //   "@/(.*)": "<rootDir>/src/$1",
-  // },
-};
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
